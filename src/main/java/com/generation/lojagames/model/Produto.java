37b060e85653801b28ae8bec287300760f286b5f
rafaelq8080 <111834369+rafaelq8080@users.noreply.github.com>
2024-02-06 01:42:34 -0300

    update

--- src/main/java/com/generation/lojagames/model/Produto.java
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
